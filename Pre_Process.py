46cfc3ce8377b141cfd3c10d61e5ac1d7c42b33d
hapuhundre <mangertim@163.com>
2017-10-24 08:39:36 +0800

    trained model

--- Pre_Process.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-
-def Norm(train,valid,test):
-    t = (train - 128) / 128
-    v = (valid - 128) / 128
-    te = (test - 128) / 128
-    return t,v,te
